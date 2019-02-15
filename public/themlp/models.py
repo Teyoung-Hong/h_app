82aa4283a6738c16c828234d1084d23f0ef35ba0
Teyoung Hong <teyon23@icloud.com>
2019-02-15 14:29:40 +0900

    model changed

--- public/themlp/models.py
+++ public/themlp/models.py
@@ -15,8 +15,18 @@ class Customer(models.Model):
         ("rk", "Rakuten mobile"),
     )
 
+    M_CHOICES = (
+        ("ntt", 'NTT'),
+        ("au", "au光"),
+        ("sb","sb光"),
+        ("hc","光コラボ"),
+        ("jc", "j:com"),
+        ("eo","eo光"),
+    )
+
     controller = models.BooleanField(default=True)
     status = models.CharField(max_length=15, verbose_name="利用状況", blank=False, null=False, choices=STATUS_CHOICES)
+    m_name = models.CharField("回線名", max_length="25", blank=True, null=True, default="わからない", choices=M_CHOICES)
     career = models.CharField(max_length=20, blank=False, null=False, choices=CAREER_CHOICES)
 
     def __str__(self):
