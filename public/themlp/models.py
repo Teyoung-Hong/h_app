6f8c341a1d888c17f9a687543671fcfb6a7912b3
Teyoung Hong <teyon23@icloud.com>
2019-02-15 14:36:19 +0900

    models change

--- public/themlp/models.py
+++ public/themlp/models.py
@@ -32,7 +32,7 @@ class Customer(models.Model):
 
     controller = models.BooleanField(default=True)
     status = models.CharField(max_length=15, verbose_name="利用状況", blank=False, null=False, choices=STATUS_CHOICES)
-    m_name = models.CharField("回線名", max_length="25", blank=True, null=True, default="わからない", choices=M_CHOICES)
+    m_name = models.CharField("回線名", max_length=25, blank=True, null=True, default="わからない", choices=M_CHOICES)
     career = models.CharField(max_length=20, verbose_name="キャリア", blank=False, null=False, choices=CAREER_CHOICES)
     money = models.CharField(max_length=20, verbose_name="現状利用価格", blank=False, null=False, choices=MONEY_CHOICES, default="")
 
